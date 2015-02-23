17f1596052bb45b1deb49f9d10e850c9f6aa21da
OHTSUKA Ko-hei <otsuka.kohei@hpsys.co.jp>
2015-02-23 13:38:32 +0900

    decimalzoom enabled

--- src/ol/interaction/pinchzoominteraction.js
+++ src/ol/interaction/pinchzoominteraction.js
@@ -118,9 +118,9 @@ ol.interaction.PinchZoom.handleUpEvent_ = function(mapBrowserEvent) {
     // Zoom to final resolution, with an animation, and provide a
     // direction not to zoom out/in if user was pinching in/out.
     // Direction is > 0 if pinching out, and < 0 if pinching in.
-    var direction = this.lastScaleDelta_ - 1;
-    ol.interaction.Interaction.zoom(map, view, resolution,
-        this.anchor_, this.duration_, direction);
+    //var direction = this.lastScaleDelta_ - 1;
+    //ol.interaction.Interaction.zoom(map, view, resolution,
+    //    this.anchor_, this.duration_, direction);
     return false;
   } else {
     return true;
